645564e0848a225859202a9784abfe3e7c7794eb
Rúben Martins <rubendavidsilvamartins@gmail.com>
2026-04-09 13:59:33 +0100

    Atualizar

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
+class VeliteWebpackPlugin {
+  static started = false;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  apply(compiler: any) {
+    compiler.hooks.beforeCompile.tapPromise("VeliteWebpackPlugin", async () => {
+      if (VeliteWebpackPlugin.started) return;
+      VeliteWebpackPlugin.started = true;
+      const dev = compiler.options.mode === "development";
+      const { build } = await import("velite");
+      await build({ watch: dev, clean: !dev });
+    });
+  }
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rmqkiclsynxcwovkteqj.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+  webpack: (config) => {
+    config.plugins.push(new VeliteWebpackPlugin());
+    return config;
+  },
 };
 
 export default nextConfig;
